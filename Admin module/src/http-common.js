c46ea62609454e56b212f5d3d80537bd50d9322e
SUPRIYABHINGE <125237738+SUPRIYABHINGE@users.noreply.github.com>
2023-08-31 19:48:53 +0530

    Delete Admin module directory

--- Admin module/src/http-common.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-  baseURL: 'http://localhost:8087/FarmersMarketplace/',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
